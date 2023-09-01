cb3b08cb3aa048d1f029f600bdb0f9abbc649d94
Millefeuille42 <mlabouri@student.42.fr>
2023-09-02 01:30:38 +0200

    feat: added context to prompter and command handler

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use std::sync::Arc;
 use tokio::select;
 use tokio::sync::Mutex;
 
-async fn command_manager(command: String, channels: IRCCommChannels<'_>) {
+async fn command_manager(command: String, channels: IRCCommChannels<'_>, ctx: &mut IRCContext) {
 	let mut command = command.trim();
 	if !command.starts_with('/') {
 		if let Err(err) = channels.write(command).await {
@@ -24,14 +24,18 @@ async fn command_manager(command: String, channels: IRCCommChannels<'_>) {
 
 	let args: Vec<&str> = command.split_whitespace().collect();
 
-	if let Err(err) = prompt_commands::commands::execute(args, channels).await {
+	if let Err(err) = prompt_commands::commands::execute(args, channels, ctx).await {
 		eprintln!("{}", err)
 	}
 }
 
-async fn prompt_user() -> String {
+async fn prompt_user(ctx: IRCContext) -> String {
 	//println!("[DEBUG] Prompting");
 	let mut input = String::new();
+	print!("{}{}",
+		if ctx.nick.is_empty() { "".to_string() } else {format!("{}@", ctx.nick)},
+		ctx.channel
+	);
 	io::stdin().read_line(&mut input).expect("Failed to read line");
 
 	return input.trim().to_string();
@@ -43,6 +47,7 @@ async fn main() {
 	let (conn_tx, conn_rx) = spawn_channel();
 	let send_rx = Arc::new(Mutex::new(send_rx));
 	let conn_rx = Arc::new(Mutex::new(conn_rx));
+	let mut context = IRCContext::new();
 
 	let mut poll_handle = start_poll_thread(send_rx, conn_rx);
 	loop {
@@ -50,13 +55,14 @@ async fn main() {
 			&send_tx,
 			&conn_tx
 		);
+		let ctx_ = context.clone();
 		select! {
 			_ = &mut poll_handle => {
 				//println!("[DEBUG] Got in poll branch");
 			},
-			input = prompt_user() => {
+			input = prompt_user(ctx_) => {
 				//println!("[DEBUG] Got in prompt branch");
-				command_manager(input, rx_channels).await;
+				command_manager(input, rx_channels, &mut context).await;
 			}
 		}
 	}
--- src/prompt_commands/commands.rs
+++ src/prompt_commands/commands.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
 use crate::prompt_commands::connect::connect;
 use crate::prompt_commands::quit::quit;
+use crate::irc::irc_context::IRCContext;
 use crate::utils::irc_comm_channels::IRCCommChannels;
 
 enum Commands {
@@ -17,10 +18,10 @@ impl Commands {
 	}
 }
 
-pub async fn execute<'a>(args: Vec<&str>, channels: IRCCommChannels<'a>) -> Result<(), Box<dyn Error>> {
-	match args.first() {
-		Some(&"connect") => Commands::Connect.execute(args, channels).await,
-		Some(&"quit") => Commands::Quit.execute(args, channels).await,
-		_ => Err("command not found!".to_string().into())
+pub async fn execute<'a>(args: Vec<&str>, channels: IRCCommChannels<'a>, ctx: &mut IRCContext) -> Result<(), Box<dyn Error>> {
+	if let None = args.first() {
+		return Err("command not found!".to_string().into())
 	}
+
+	Commands::new(args.first().unwrap()).execute(args, channels, ctx).await
 }
