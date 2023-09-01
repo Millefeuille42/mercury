1a27a9649fde61de6de69ae4a1296cb31b39adf0
Millefeuille42 <mlabouri@student.42.fr>
2023-09-02 01:27:41 +0200

    feat: added new and clone functions to IRCContext

--- src/irc/irc_context.rs
+++ src/irc/irc_context.rs
@@ -1,6 +1,27 @@
+// TODO add connected status
 pub struct IRCContext {
-	server: String,
-	nick: String,
-	password: String,
-	channel: String,
+	pub server: String,
+	pub nick: String,
+	pub password: String,
+	pub channel: String,
+}
+
+impl IRCContext {
+	pub fn new() -> Self {
+		IRCContext {
+			server: "".to_string(),
+			nick: "".to_string(),
+			password: "".to_string(),
+			channel: "".to_string(),
+		}
+	}
+
+	pub fn clone(&mut self) -> Self {
+		IRCContext {
+			server: self.server.clone(),
+			nick: self.nick.clone(),
+			password: self.password.clone(),
+			channel: self.channel.clone(),
+		}
+	}
 }
