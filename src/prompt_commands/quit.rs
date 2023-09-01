fe8aa40cbfccb5808b2810cf3b0529d9d215618a
Millefeuille42 <mlabouri@student.42.fr>
2023-09-02 01:26:53 +0200

    fix: added clap to quit command

--- src/prompt_commands/quit.rs
+++ src/prompt_commands/quit.rs
@@ -1,7 +1,11 @@
 use std::error::Error;
+use clap::{ArgMatches, Command};
 use crate::utils::irc_comm_channels::IRCCommChannels;
 
-pub async fn quit<'a>(_: Vec<&str>, channels: IRCCommChannels<'a>) -> Result<(), Box<dyn Error>> {
+pub async fn quit<'a>(args: Vec<&str>, channels: IRCCommChannels<'a>) -> Result<(), Box<dyn Error>> {
+	let _: ArgMatches = Command::new("join")
+		.try_get_matches_from_mut(args)?;
+
 	channels.disconnect().await?;
 	Ok(())
 }
